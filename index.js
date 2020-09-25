b8f2058f416d14dd04e4ace031b7276b1d688593
simottardi <simottardi@gmail.com>
2020-09-25 11:56:43 +0200

    added categories route with the products! yey

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ app.use(jsonParser);
 const userRouter = require("./routers/user");
 const orderRouter = require("./routers/order");
 const productRouter = require("./routers/product");
+const categoryRouter = require("./routers/category");
 
 app.use("/users", userRouter);
 app.use("/orders", orderRouter);
 app.use("/products", productRouter);
+app.use("/categories", categoryRouter);
 // app.use("/images", imageRouter);
 // app.use("/auth", authRouter);
 
